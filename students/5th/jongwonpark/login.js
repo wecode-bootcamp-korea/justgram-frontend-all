ed85be8c6fb1c0ac0ea2645f99763e3d09a07ac6
mulnon2g <carryy333@gmail.com>
2022-05-18 13:40:05 +0900

    login완료 메인인nav완료

---
+++ students/5th/jongwonpark/login.js
@@ -0,0 +1,9 @@
+const inputjs= document.getElementsByClassName('inputhtml')[0];
+const buttontag = document.getElementsByClassName('login')[0];
+function myFunction() {
+     buttontag.style.opacity = '1';
+  }
+inputjs.addEventListener("keyup",myFunction);
+
+
+
