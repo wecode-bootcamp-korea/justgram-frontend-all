70f883b017afe1e570b760b84d5c19fe5827aa59
azure928 <azure6.dev@gmail.com>
2022-05-19 02:33:07 +0900

    Add: 미션 2 js 파일작성 id, pw 에 각각 한 글자 이상 써야 버튼이 활성화되도록 구현

---
+++ students/5th/jiyook/js/login.js
@@ -0,0 +1,27 @@
+
+const inputId = document.querySelector('#id');
+const inputPw = document.querySelector('#pw');
+const button = document.querySelector('.loginBtn');
+
+
+
+function loginBtn() {
+    //console.log(inputId.value);
+    //console.log(typing);
+
+    
+    let idValue = inputId.value;
+    let passwordValue = inputPw.value;
+
+    if(idValue.length > 0 && passwordValue.length > 0) {
+        console.log("테스트!!!");
+        button.style.opacity = 1;
+        button.disabled = false;
+    }else {
+        button.style.opacity = 0.5;
+        button.disabled = true;
+    }
+};
+
+inputId.addEventListener('keyup', loginBtn);
+inputPw.addEventListener('keyup', loginBtn);                       
