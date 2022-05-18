be5a30cbc14c45bc4a17c1148c8ed033ba8fa572
happyeveryone96 <66675699+happyeveryone96@users.noreply.github.com>
2022-05-18 14:39:37 +0900

    Add: ID,PW 입력 시 로그인 버튼 활성화 기능 구현

---
+++ students/5th/jinwoo/login.js
@@ -0,0 +1,49 @@
+const idInput = document.getElementById('id-input');
+const pwInput = document.getElementById('pw-input');
+const loginBtn = document.getElementById('login-btn');
+
+loginBtn.disabled = true;
+let checkIdInput = false;
+let checkPwInput = false;
+
+
+const checkId = () => {
+  if (idInput.value !== '') {
+    checkIdInput = true;
+  } else {
+    checkIdInput = false;
+  }
+}
+
+const checkPw = () => {
+  if (pwInput.value !== '') {
+    checkPwInput = true;
+  } else {
+    checkPwInput = false;
+  }
+}
+
+idInput.addEventListener('keyup', checkId);
+pwInput.addEventListener('keyup', checkPw);
+
+
+const changeBtnState = () => {
+  if (checkIdInput === true && checkPwInput === true) {
+    loginBtn.style.background = 'rgb(115,180,226)';
+    loginBtn.disabled = false;
+    loginBtn.style.cursor = 'pointer';
+  } else {
+    loginBtn.style.background = 'rgb(172,212,234)';
+    loginBtn.disabled = true;
+    loginBtn.style.cursor = 'auto';
+  }
+}
+
+idInput.addEventListener('keyup', changeBtnState);
+pwInput.addEventListener('keyup', changeBtnState);
+
+const moveToListPage = () => {
+  location.href="list.html";
+}
+
+loginBtn.addEventListener('click', moveToListPage); 
