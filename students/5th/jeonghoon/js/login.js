12a2d4bef203b6135f7c52c9ccba3c75c7a01ad7
Choi-jeonghoon <68211978+Choi-jeonghoon@users.noreply.github.com>
2022-05-19 11:35:07 +0900

    수정

--- students/5th/jeonghoon/js/login.js
+++ students/5th/jeonghoon/js/login.js
@@ -1,18 +1,3 @@
-/*function loginChk() {
-  var id = document.getElementById("id");
-  var pw = document.getElementById("pw");
-
-  if (!id.value) {
-    alert("아이디를 입력해 주십시오.");
-    return;
-  }
-
-  if (!pw.value) {
-    alert("비밀번호를 입력해 주십시오.");
-    return;
-  }
-}
-*/
 let id = document.getElementById("id");
 let password = document.getElementById("pw");
 let loginChk = document.getElementsByClassName("login-btn")[0];
