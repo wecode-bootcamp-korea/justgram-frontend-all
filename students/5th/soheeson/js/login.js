837ca5367856864cb7ddbc42d58591c6985ee1e2
손소희 <sonsohee@sonsohuis-MacBook-Air.local>
2022-05-20 04:00:18 +0900

    Add: 메인 페이지 레이아웃

--- students/5th/soheeson/js/login.js
+++ students/5th/soheeson/js/login.js
@@ -1,5 +1,36 @@
 // id와 pw가 모두 공백이 아니면 버튼 클릭 가능 , 컬러:블루
 // id와 pw가 모두 공백이 아닌게 아니면(공백이면) 버튼 클릭 불가 , 컬러:스카이블루
+/*
+console.log("javascript 실행")
+function checkValidation(idText, pwText) {
+    if (!idText.length === 0 || pwText.length === 0 ) {
+        return false;
+    }
+    if(pwText.length < 8) {
+    }
+    return true;
+}
+
+const idInput = document.getElementsByClassName('id')[0];
+const pwInput = document.getElementsByClassName('pw')[0];
+const loginBoxButton = document.getElementsByClassName('login-button')[0];
+
+idInput.addEventListener('input', (event) =>  {
+    const idText = document.getElementsByClassName("id")
+    const pwText = document.getElementsByClassName("pw")
+    if (checkValidation(idText, pwText)) {
+        const loginBtn = doucument.getElementsByClassName
+    }
+});
+
+pwInput.addEventListener('input', (event) =>  {
+    const idText = document.getElementsByClassName("id")
+    const pwText = document.getElementsByClassName("pw")
+});
+
+function
+*/
+
 
 const idInput = document.getElementsByClassName('id')[0];
 const pwInput = document.getElementsByClassName('pw')[0];
@@ -17,6 +48,3 @@ function buttonColorChange() {
         loginBoxButton.style.backgroundColor = '#c4e1fb';
     }
 };
-
-
-
