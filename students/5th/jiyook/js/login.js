a6b42f1c2c8eae322435f23e2a62a7aa90f841ce
azure928 <azure6.dev@gmail.com>
2022-05-19 19:16:56 +0900

    Add: 메인 페이지 대략적인 레이아웃 -메인 페이지 큰 요소들의 위치만 잡아놓음

--- students/5th/jiyook/js/login.js
+++ students/5th/jiyook/js/login.js
@@ -1,27 +1,24 @@
-
 const inputId = document.querySelector('#id');
 const inputPw = document.querySelector('#pw');
 const button = document.querySelector('.loginBtn');
 
-
-
 function loginBtn() {
-    //console.log(inputId.value);
-    //console.log(typing);
+  //console.log(inputId.value);
+  //console.log(typing);
 
-    
-    let idValue = inputId.value;
-    let passwordValue = inputPw.value;
+  let idValue = inputId.value;
+  let passwordValue = inputPw.value;
 
-    if(idValue.length > 0 && passwordValue.length > 0) {
-        console.log("테스트!!!");
-        button.style.opacity = 1;
-        button.disabled = false;
-    }else {
-        button.style.opacity = 0.5;
-        button.disabled = true;
-    }
-};
+  if (idValue.length > 0 && passwordValue.length > 0) {
+    console.log('테스트!!!');
+    button.style.opacity = 1;
+    button.disabled = false;
+  } else {
+    button.style.opacity = 0.5;
+    button.disabled = true;
+    //button.style.cursor = 'poninter';
+  }
+}
 
 inputId.addEventListener('keyup', loginBtn);
-inputPw.addEventListener('keyup', loginBtn);                       
+inputPw.addEventListener('keyup', loginBtn);
