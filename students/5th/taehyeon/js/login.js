5dfcb4d929552586e3d54a3361a5173018bab4de
taebong20 <taebong3090@gmail.com>
2022-05-20 09:09:00 +0900

    수정

--- students/5th/taehyeon/js/login.js
+++ students/5th/taehyeon/js/login.js
@@ -1,11 +1,1 @@
-const id = document.querySelector(".input_id");
-const pw = document.querySelector(".input_pass");
-const btn = document.querySelector(".login_btn");
-
-id.addEventListener("change", btnHandler);
-
-function btnHandler(params) {
-  if (id.value && pw.value) {
-    btn.disabled = false;
-  }
-}
+const idInput = document.getElementsByClassName("idetu")
