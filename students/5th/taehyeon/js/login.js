1869d6d7da2c8325dbfd06e0100436a69ebc54b0
taebong20 <taebong3090@gmail.com>
2022-05-20 21:05:57 +0900

    미션2 등록

--- students/5th/taehyeon/js/login.js
+++ students/5th/taehyeon/js/login.js
@@ -1,1 +1,34 @@
-const idInput = document.getElementsByClassName("idetu")
+function checkValidation(idText, pwText) {
+  //   if (!idText.includes("@")) {
+  //     return false;
+  //   }
+  //   if (pwText.length >= 8) {
+  //     return false;
+  //   }
+  return true;
+}
+
+function loginBtnActive() {
+  const loginBtn = docoument.getElementsByClassName("login-btn")[0];
+  loginBtn.style.backgroundColor = "#61ADED";
+  loginBtn.style.cursor = "pointer";
+}
+
+const idInput = document.getElementsByClassName("identify")[0];
+const pwInput = document.getElementsByClassName("password")[0];
+
+idInput.addEventListener("input", () => {
+  const idText = document.getElementsByClassName("identify")[0].value;
+  const pwText = document.getElementsByClassName("password")[0].value;
+  if (checkValidation(idText, pwText)) {
+    loginBtnActive();
+  }
+});
+
+pwInput.addEventListener("input", () => {
+  const idText = document.getElementsByClassName("identify")[0].value;
+  const pwText = document.getElementsByClassName("password")[0].value;
+  if (checkValidation(idText, pwText)) {
+    loginBtnActive();
+  }
+});
