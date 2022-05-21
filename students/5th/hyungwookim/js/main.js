ebde15197bc9f36d751a59a0ce387e4ffa68d278
noondaydream <104835917+thenoondaydream@users.noreply.github.com>
2022-05-21 20:36:00 +0900

    Add: 메인 페이지 레이아웃 구현

---
+++ students/5th/hyungwookim/js/main.js
@@ -0,0 +1,12 @@
+const inputComment = document.getElementsByClassName("comment_input")[0];
+
+
+function createCommentDiv(writer, content){
+    const commentDiv = document.createElement("div");
+
+    
+    const writerSpan = document.createElement("span");
+    const writerSpan = document.createElement("span");
+
+    commentDiv.append();
+}
