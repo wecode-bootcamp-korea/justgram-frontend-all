b0d3f2a1e1d92232fae37fb2976dfe0cee116642
noondaydream <104835917+thenoondaydream@users.noreply.github.com>
2022-05-21 21:30:21 +0900

    Add: 댓글 내용 입력 후 Enter press, 혹은 게시 버튼 클릭 시 댓글 추가 기능 구현, CSS  페이지 하단 마진(40px) 추가

--- students/5th/hyungwookim/js/main.js
+++ students/5th/hyungwookim/js/main.js
@@ -1,12 +1,46 @@
-const inputComment = document.getElementsByClassName("comment_input")[0];
 
+const inputComments = document.getElementsByClassName("comment_input");
 
-function createCommentDiv(writer, content){
-    const commentDiv = document.createElement("div");
+Array.from(inputComments).forEach((inputComment, index) => {
+  inputComment.addEventListener("keyup", (event) => {
+    if (event.code === "Enter") {
+      writeComment(index);
+    }
+  });
+});
 
-    
-    const writerSpan = document.createElement("span");
-    const writerSpan = document.createElement("span");
+const writeCommentButtons = document.getElementsByClassName("write_comment_button");
 
-    commentDiv.append();
+Array.from(writeCommentButtons).forEach((writeCommentButton, index) => {
+  writeCommentButton.addEventListener("click", () => {
+    writeComment(index);
+  });
+});
+
+// 여기까지 이벤트 감지.
+
+function writeComment(index) {
+  const commentInput = document.getElementsByClassName("comment_input")[index];
+  const commentDiv = createCommentDiv("hyungwookim", commentInput.value);
+  const commentGroup = document.getElementsByClassName("comment_group")[index];
+  commentGroup.appendChild(commentDiv);
+  commentInput.value = "";
+}
+
+function createCommentDiv(writer, content) {
+  const commentDiv = document.createElement("div");
+  commentDiv.setAttribute("class","new_comment")
+  const writerSpan = document.createElement("span"); // <span></span>
+  const writerText = document.createTextNode(writer);
+  writerSpan.setAttribute("class", "new_comment_text")
+  writerSpan.appendChild(writerText); // <span>writer</span>
+
+  const commentSpan = document.createElement("span");
+  const commentText = document.createTextNode(content);
+  commentSpan.appendChild(commentText); // <span>content</span>
+
+  commentDiv.append(writerSpan, " ", commentSpan);
+  // <div><span>writer</span> <span>content</span></div>
+
+  return commentDiv;
 }
