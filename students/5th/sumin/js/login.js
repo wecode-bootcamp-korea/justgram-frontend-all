7178e4d8b317cc3811cdf1b982ed97a8484fef2c
ssm825 <ssm39825@gmail.com>
2022-05-24 00:37:02 +0900

    Add : main.html, main.css, main.js, reset.css  file 추가, images 폴더 추가  Modify : css 불필요 코드 정리, html, css 클래스명 수정

--- students/5th/sumin/js/login.js
+++ students/5th/sumin/js/login.js
@@ -1,18 +1,18 @@
 // 로그인 버튼 활성 & 비활성화
 
-const loginBtn = document.getElementsByClassName("loginBtn")[0];
-const loginId = document.getElementsByClassName("loginId")[0];
-const loginPw = document.getElementsByClassName("loginPw")[0];
+const login__btn = document.getElementsByClassName("login__btn")[0];
+const login__id = document.getElementsByClassName("login__id")[0];
+const login__pw = document.getElementsByClassName("login__pw")[0];
 
 function btnEvent(){
- if (loginId.value && loginPw.value) {
-  loginBtn.removeAttribute("disabled");
-  loginBtn.style.backgroundColor = ("rgba(0,149,246,1)");
- } else if (loginId.value || loginPw.value) {
-  loginBtn.getAttribute("disabled");
-  loginBtn.style.backgroundColor = ("rgba(0,149,246,.3)");
+ if (login__id.value && login__pw.value) {
+  login__btn.removeAttribute("disabled");
+  login__btn.style.backgroundColor = ("rgba(0,149,246,1)");
+ } else if (login__id.value || login__pw.value) {
+  login__btn.getAttribute("disabled");
+  login__btn.style.backgroundColor = ("rgba(0,149,246,.3)");
  }
 };
 
-loginId.addEventListener('keyup',btnEvent);
-loginPw.addEventListener('keyup',btnEvent);
+login__id.addEventListener('keyup',btnEvent);
+login__pw.addEventListener('keyup',btnEvent);
